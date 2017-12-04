c09796638231b4936731438df8319f50d5696a07
KyuWan Kim <lpnids@gmail.com>
2017-12-04 15:11:02 +0900

    Initial commit

---
+++ app/src/main/java/com/example/dataking/googleloginkotlin/MainActivity.kt
@@ -0,0 +1,72 @@
+package com.example.dataking.googleloginkotlin
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.common.api.ResultCallback
+import com.google.android.gms.common.api.Status
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), GoogleApiClient.OnConnectionFailedListener {
+    override fun onConnectionFailed(connectionResult: ConnectionResult) {
+        Log.d("LOGIN", "======================================================================== GOOGLE LOGIN FAIL")
+    }
+
+    private val RC_SIGN_IN = 9001
+    private var mGoogleApiClient: GoogleApiClient? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        updateUI(false)
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN).requestEmail().build()
+
+        mGoogleApiClient = GoogleApiClient.Builder(this).enableAutoManage(this, this).addApi(Auth.GOOGLE_SIGN_IN_API, gso).build()
+
+        sign_in_button.setOnClickListener(View.OnClickListener {
+
+            var signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient)
+            startActivityForResult(signInIntent, RC_SIGN_IN)
+        })
+
+        btn_logout.setOnClickListener(View.OnClickListener {
+            Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback {
+                object : ResultCallback<Status> {
+                    override fun onResult(status: Status) {
+                        updateUI(status.isSuccess)
+                    }
+                }
+            }
+        })
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            var result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+            updateUI(result.isSuccess)
+        }
+    }
+
+    private fun updateUI(isLogin: Boolean) {
+        if (isLogin) {
+            sign_in_button.visibility = View.GONE
+            btn_logout?.visibility = View.VISIBLE
+        } else {
+            sign_in_button?.visibility = View.VISIBLE
+            btn_logout?.visibility = View.GONE
+        }
+    }
+
+
+}
