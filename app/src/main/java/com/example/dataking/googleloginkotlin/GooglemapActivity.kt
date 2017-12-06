d14c8c628b3927c281805c55ea16d86d210ca3fb
kyuwankim <lpnids@gmail.com>
2017-12-06 21:51:14 +0900

    Add Googlemap Activity

---
+++ app/src/main/java/com/example/dataking/googleloginkotlin/GooglemapActivity.kt
@@ -0,0 +1,12 @@
+package com.example.dataking.googleloginkotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class GooglemapActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_googlemap)
+    }
+}
