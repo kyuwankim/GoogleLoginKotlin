8259e4134b249486300103615a93b562d4278654
KyuWan Kim <lpnids@gmail.com>
2017-12-13 12:50:25 +0900

    add signup activity

--- app/src/main/java/com/example/dataking/googleloginkotlin/MainActivity.kt
+++ app/src/main/java/com/example/dataking/googleloginkotlin/MainActivity.kt
@@ -1,14 +1,23 @@
 package com.example.dataking.googleloginkotlin
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        txt_signupnow.setOnClickListener(View.OnClickListener {
+            var intent : Intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+        })
 
     }
 }
---
+++ app/src/main/java/com/example/dataking/googleloginkotlin/SignupActivity.kt
@@ -0,0 +1,17 @@
+package com.example.dataking.googleloginkotlin
+
+import android.graphics.Color
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.Toolbar
+import android.view.View
+
+class SignupActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+
+
+    }
+}
