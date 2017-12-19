2bbd44188d6dbeb40f4989f014856cc31d3120cc
KyuWan Kim <lpnids@gmail.com>
2017-12-19 11:59:18 +0900

    complete get Google Token

--- app/src/main/java/com/example/dataking/googleloginkotlin/MainActivity.kt
+++ app/src/main/java/com/example/dataking/googleloginkotlin/MainActivity.kt
@@ -3,19 +3,30 @@ package com.example.dataking.googleloginkotlin
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.View
+import android.widget.ImageButton
+import android.widget.Toast
+import com.google.android.gms.auth.api.Auth
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.auth.api.signin.GoogleSignInResult
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), GoogleApiClient.OnConnectionFailedListener {
+    override fun onConnectionFailed(p0: ConnectionResult) {
+        Toast.makeText(this, "Login failed", Toast.LENGTH_LONG).show()
+    }
+
+    private var mGoogleApiClient: GoogleApiClient? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        gooogle()
-
+        initGoogle()
 
         txt_signupnow.setOnClickListener(View.OnClickListener {
             var intent: Intent = Intent(this, ListActivity::class.java)
@@ -24,11 +35,54 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun gooogle() {
+    private fun initGoogle() {
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .requestIdToken(getString(R.string.server_client_id))
+                .build()
+        mGoogleApiClient = GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build()
 
-        val gso : GoogleSignInOptions  = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN).requestEmail().build()
 
+        val signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient)
+        startActivityForResult(signInIntent, RC_SIGN_IN)
     }
 
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+            handleSignInResult(result)
+        }
+
+    }
+
+    private fun handleSignInResult(result: GoogleSignInResult) {
+        if (result.isSuccess) {
+
+
+            val acct = result.signInAccount
+
+            val name: String
+            if (acct != null) {
+                name = acct.givenName + " " + acct.familyName
+
+                var userImg = ""
+                if (acct.photoUrl != null) {
+                    userImg = acct.photoUrl!!.toString()
+                }
+                Log.d("IntroActivity", acct.idToken)
+            }
+        }
+    }
+
+
+    companion object {
+
+        private val RC_SIGN_IN = 9001
+    }
 }
